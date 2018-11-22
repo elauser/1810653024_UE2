12f00260dbb37878baa333de91585d600469b26f
elauser <1810653024@stud.fh-kufstein.ac.at>
2018-11-22 20:54:53 +0100

    Aufgabe 4

---
+++ src/Aufgabe4_1810653024.java
@@ -0,0 +1,18 @@
+public class Aufgabe4_1810653024 {
+    public static void main(String[] args){
+        for(int i = 1; i < 11;i++){
+            System.out.println("Der Schleifenzähler ist " + i);
+        }
+        int whilezähler = 1;
+        while (whilezähler < 11){
+            System.out.println("Der Schleifenzähler ist " + whilezähler);
+            whilezähler++;
+        }
+        whilezähler = 1;
+        do {
+            System.out.println("Der Schleifenzähler ist " + whilezähler);
+            whilezähler++;
+        } while(whilezähler < 11);
+
+    }
+}
